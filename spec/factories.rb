29cc2a1da488a1e9844d4724ee6c91ac8aa13b23
Brian Strezze <bstrezze@gosmcpartners.com>
2013-12-18 12:57:22 -0500

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Brian Strezze"
+    email    "bstrezze@gosmcpartners.com"
+    password "MayHem01"
+    password_confirmation "MayHem01"
+  end
+end
