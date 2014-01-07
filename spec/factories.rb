c040d200b7e0ae81dc38d8d080732f1e5fdd0fb1
Brian Strezze <bstrezze@gosmcpartners.com>
2014-01-07 16:58:48 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Brian Strezze"
-    email    "bstrezze@gosmcpartners.com"
-    password "MayHem01"
-    password_confirmation "MayHem01"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
